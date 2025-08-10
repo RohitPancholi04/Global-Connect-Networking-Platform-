789f1a0acc1313a73ccd3c7b8dab82e21cadff18
Muskan06-star <shrivastavamuskan45@gmail.com>
2025-08-10 13:17:42 +0530

    add some files

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const protect = require('../middleware/authMiddleware');
 const multer = require('multer');
+const Post = require("../models/Post");
+const upload = require("../middleware/uploadMiddleware");
 const { createPost, getFeed, likePost, commentPost } = require('../controllers/postController');
-
+const auth = require("../middleware/authMiddleware.js");
 const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
-router.post('/', protect, upload.single('image'), createPost);
-router.get('/feed', protect, getFeed);
+router.post('/', auth, upload.single('image'), createPost);
+router.get('/feed', auth, getFeed);
 router.put('/:id/like', protect, likePost);
 router.post('/:id/comment', protect, commentPost);
 
