8a040f93ea61f6627469a3b4762c804c3bf3dd5c
RohitPancholi04 <rs0662217@gmail.com>
2025-08-12 16:53:37 +0530

    admin Panel created

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
 app.use('/api/messages', require('./routes/messageRoutes'));
+app.use('/api/admin',require('./routes/adminRoutes'))
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
