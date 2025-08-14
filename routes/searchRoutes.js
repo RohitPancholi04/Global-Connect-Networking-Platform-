2c15981bfb43a4aefbfb0be3cf3b06d10088ae5d
RohitPancholi04 <rs0662217@gmail.com>
2025-08-14 23:40:22 +0530

    fullStack

--- routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/searchRoutes.js
-const express = require("express");
-const router = express.Router();
-const { globalSearch } = require("../controllers/searchController");
-
-router.get("/", globalSearch);
-
-module.exports = router;
