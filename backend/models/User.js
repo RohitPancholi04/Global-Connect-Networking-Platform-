95b711369498d1cdeb8dd87b68f771f3894ad1a2
RohitPancholi04 <rs0662217@gmail.com>
2025-08-14 23:49:58 +0530

    Added backend folder as normal directory

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bio: { type: String, default: '' },
+  skills: { type: [String], default: [] },
+  location: { type: String, default: '' },
+  profilePic: { type: String, default: 'https://tse3.mm.bing.net/th/id/OIP.1waDZ8Q2eWBkenMscI08qAHaHa?pid=Api&P=0&h=180' },
+  company: { type: String, default: '' },
+  connections: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  pendingRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+const educationSchema = new mongoose.Schema({
+  school: String,
+  degree: String,
+  fieldOfStudy: String,
+  startDate: Date,
+  endDate: Date,
+  description: String
+});
+const experienceSchema = new mongoose.Schema({
+  company: String,
+  position: String,
+  startDate: Date,
+  endDate: Date,
+  description: String
+});
+
+
+module.exports = mongoose.model('User', userSchema),
+ mongoose.model('Education', educationSchema),
+  mongoose.model('Experience', experienceSchema);
