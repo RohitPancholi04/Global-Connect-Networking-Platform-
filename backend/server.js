5f60986b346338d56cc8cd14d12c6b777a21edc1
Rohit Sharma <rs0662217@gmail.com>
2025-08-18 19:54:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectCloudinary();
 
 
 //Middleware 
-app.use(cors({origin: "https://global-connect-networking-platform-br3m.onrender.com", credentials: true}));
+app.use(cors({origin: "https://global-connect-networking-platform-doty.onrender.com", credentials: true}));
 app.use(express.json({ limit: "10mb" }));
 
 // Routes
